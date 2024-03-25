5fd74681897f2a2d704c1a06c6c5cd192c480ab8
Tomas Paez <paeztms@gmail.com>
2024-03-24 22:33:24 -0300

    card is zoomed on hover

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,172 +4,186 @@ import Card from './Card';
 import type { MagicApiCardResponse } from 'src/types';
 
 document.addEventListener('contextmenu', event => {
-	event.preventDefault();
+  event.preventDefault();
 });
 
 function Board() {
-	const boardRef = React.useRef<HTMLDivElement>(null);
-	const [cards, setCards] = React.useState<MagicApiCardResponse[]>([]);
-	const [selectedCards, setSelectedCards] = React.useState<string[]>([]);
-	const [dragStartPositions, setDragStartPositions] = React.useState<{
-		[key: string]: { x: number; y: number };
-	}>();
+  const boardRef = React.useRef<HTMLDivElement>(null);
+  const [cards, setCards] = React.useState<MagicApiCardResponse[]>([]);
+  const [selectedCards, setSelectedCards] = React.useState<string[]>([]);
+  const [dragStartPositions, setDragStartPositions] = React.useState<{
+    [key: string]: { x: number; y: number };
+  }>();
 
-	const [isDragging, setIsDragging] = React.useState(false);
-	const [dragStart, setDragStart] = React.useState({ x: 0, y: 0 });
-	const [dragOffset, setDragOffset] = React.useState({ x: 0, y: 0 });
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
-	const [isSelecting, setIsSelecting] = React.useState(false);
-	const [selectionArea, setSelectionArea] = React.useState({
-		startX: 0,
-		startY: 0,
-		endX: 0,
-		endY: 0,
-	});
+  const [isDragging, setIsDragging] = React.useState(false);
+  const [dragStart, setDragStart] = React.useState({ x: 0, y: 0 });
+  const [dragOffset, setDragOffset] = React.useState({ x: 0, y: 0 });
 
-	const handleMouseDown = (e: React.MouseEvent) => {
-		if (!(e.target instanceof HTMLDivElement)) {
-			return;
-		}
-		if (e.target.dataset.element === 'CARD') {
-			setIsDragging(true);
-			setDragStart({ x: e.clientX, y: e.clientY });
+  const [isSelecting, setIsSelecting] = React.useState(false);
+  const [selectionArea, setSelectionArea] = React.useState({
+    startX: 0,
+    startY: 0,
+    endX: 0,
+    endY: 0,
+  });
 
-			// Store the initial positions of the selected cards
-			const newDragStartPositions: typeof dragStartPositions = {};
-			selectedCards.forEach(cardId => {
-				const cardElement = document.getElementById(cardId);
-				if (cardElement) {
-					const cardRect = cardElement.getBoundingClientRect();
-					newDragStartPositions[cardId] = { x: cardRect.left, y: cardRect.top };
-				}
-			});
-			setDragStartPositions(newDragStartPositions);
-		} else if (e.target.dataset.element === 'BOARD') {
-			setIsSelecting(true);
-			const { clientX, clientY } = e;
-			setSelectionArea({
-				startX: clientX,
-				startY: clientY,
-				endX: clientX,
-				endY: clientY,
-			});
-		}
-	};
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (!(e.target instanceof HTMLDivElement)) {
+      return;
+    }
+    if (e.target.dataset.element === 'CARD') {
+      setIsDragging(true);
+      setDragStart({ x: e.clientX, y: e.clientY });
 
-	const handleMouseMove = (e: React.MouseEvent) => {
-		if (isSelecting) {
-			const { clientX, clientY } = e;
-			setSelectionArea(prev => ({ ...prev, endX: clientX, endY: clientY }));
-		} else if (isDragging && selectedCards.length > 0) {
-			const deltaX = e.clientX - dragStart.x;
-			const deltaY = e.clientY - dragStart.y;
-			setDragOffset({ x: deltaX, y: deltaY });
-		}
-	};
+      // Store the initial positions of the selected cards
+      const newDragStartPositions: typeof dragStartPositions = {};
+      selectedCards.forEach(cardId => {
+        const cardElement = document.getElementById(cardId);
+        if (cardElement) {
+          const cardRect = cardElement.getBoundingClientRect();
+          newDragStartPositions[cardId] = { x: cardRect.left, y: cardRect.top };
+        }
+      });
+      setDragStartPositions(newDragStartPositions);
+    } else if (e.target.dataset.element === 'BOARD') {
+      setIsSelecting(true);
+      const { clientX, clientY } = e;
+      setSelectionArea({
+        startX: clientX,
+        startY: clientY,
+        endX: clientX,
+        endY: clientY,
+      });
+    }
+  };
 
-	const handleMouseUp = () => {
-		setIsSelecting(false);
-		setIsDragging(false);
-		logSelectedCards();
-	};
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (isSelecting) {
+      const { clientX, clientY } = e;
+      setSelectionArea(prev => ({ ...prev, endX: clientX, endY: clientY }));
+    } else if (isDragging && selectedCards.length > 0) {
+      const deltaX = e.clientX - dragStart.x;
+      const deltaY = e.clientY - dragStart.y;
+      setDragOffset({ x: deltaX, y: deltaY });
+    }
+  };
 
-	const logSelectedCards = () => {
-		if (!boardRef.current) return;
+  const handleMouseUp = () => {
+    isSelecting && setIsSelecting(false);
+    isDragging && setIsDragging(false);
+    logSelectedCards();
+  };
 
-		const boardRect = boardRef.current.getBoundingClientRect();
-		const newSelectedCards = cards.filter(card => {
-			const cardRect = document
-				.getElementById(card.id)
-				?.getBoundingClientRect();
+  const logSelectedCards = () => {
+    if (!boardRef.current) return;
 
-			if (!cardRect) return false;
+    const boardRect = boardRef.current.getBoundingClientRect();
+    const newSelectedCards = cards.filter(card => {
+      const cardRect = document
+        .getElementById(card.id)
+        ?.getBoundingClientRect();
 
-			const cardLeft = cardRect.left - boardRect.left;
-			const cardTop = cardRect.top - boardRect.top;
-			const cardRight = cardRect.right - boardRect.left;
-			const cardBottom = cardRect.bottom - boardRect.top;
+      if (!cardRect) return false;
 
-			const selectionLeft = Math.min(selectionArea.startX, selectionArea.endX);
-			const selectionTop = Math.min(selectionArea.startY, selectionArea.endY);
-			const selectionRight = Math.max(selectionArea.startX, selectionArea.endX);
-			const selectionBottom = Math.max(
-				selectionArea.startY,
-				selectionArea.endY
-			);
+      const cardLeft = cardRect.left - boardRect.left;
+      const cardTop = cardRect.top - boardRect.top;
+      const cardRight = cardRect.right - boardRect.left;
+      const cardBottom = cardRect.bottom - boardRect.top;
 
-			return (
-				cardLeft >= selectionLeft &&
-				cardTop >= selectionTop &&
-				cardRight <= selectionRight &&
-				cardBottom <= selectionBottom
-			);
-		});
+      const selectionLeft = Math.min(selectionArea.startX, selectionArea.endX);
+      const selectionTop = Math.min(selectionArea.startY, selectionArea.endY);
+      const selectionRight = Math.max(selectionArea.startX, selectionArea.endX);
+      const selectionBottom = Math.max(
+        selectionArea.startY,
+        selectionArea.endY
+      );
 
-		setSelectedCards(newSelectedCards.map(card => card.id));
-		console.log('Selected Cards:', newSelectedCards);
-	};
+      return (
+        cardLeft >= selectionLeft &&
+        cardTop >= selectionTop &&
+        cardRight <= selectionRight &&
+        cardBottom <= selectionBottom
+      );
+    });
 
-	React.useEffect(() => {
-		async function getCards() {
-			try {
-				const response = await fetch('public/magic_api_response.json');
-				const data = await response.json();
-				setCards(data.cards);
-			} catch (error) {
-				console.log(error);
-			}
-		}
+    setSelectedCards(newSelectedCards.map(card => card.id));
+    console.log('Selected Cards:', newSelectedCards);
+  };
 
-		getCards();
-	}, []);
+  function handleKeyDown(e: React.KeyboardEvent) {
+    console.log(e.key);
+    if (e.key === 'Escape') {
+      setSelectedCards([]);
+    } else if (e.key === 'm') {
+      setMenuOpen(!menuOpen);
+    }
+  }
 
-	return (
-		<div
-			data-element="BOARD"
-			ref={boardRef}
-			onMouseDown={handleMouseDown}
-			onMouseMove={handleMouseMove}
-			onMouseUp={handleMouseUp}
-			style={{
-				height: 'calc(100vh - 17px )',
-				width: '200vw',
-				overflowY: 'hidden',
-				backgroundColor: 'hsl(0, 0%, 50%)',
-				position: 'relative',
-			}}
-		>
-			{isSelecting && (
-				<div
-					style={{
-						position: 'absolute',
-						border: '1px solid yellowgreen',
-						backgroundColor: 'rgba(0, 0, 0, 0.1)',
-						left: Math.min(selectionArea.startX, selectionArea.endX),
-						top: Math.min(selectionArea.startY, selectionArea.endY),
-						width: Math.abs(selectionArea.endX - selectionArea.startX),
-						height: Math.abs(selectionArea.endY - selectionArea.startY),
-					}}
-				/>
-			)}
-			<div>
-				{cards.length > 0 &&
-					cards.slice(0, 23).map((card, index) => {
-						return (
-							<Card
-								key={card.id + index}
-								card={card}
-								initialCoordinates={{ x: 5, y: 470 }}
-								index={index}
-								isSelected={selectedCards.includes(card.id)}
-								dragOffset={dragOffset}
-								dragStartPositions={dragStartPositions}
-							/>
-						);
-					})}
-			</div>
-		</div>
-	);
+  React.useEffect(() => {
+    async function getCards() {
+      try {
+        const response = await fetch('public/magic_api_response.json');
+        const data = await response.json();
+        setCards(data.cards);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getCards();
+  }, []);
+
+  return (
+    <div
+      tabIndex={0}
+      data-element="BOARD"
+      ref={boardRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onKeyDown={handleKeyDown}
+      style={{
+        height: '200vh',
+        width: '96vw',
+        margin: '10px',
+        backgroundColor: 'hsl(0, 0%, 50%)',
+        position: 'relative',
+      }}
+    >
+      {/* Dragging click down animation */}
+      {isSelecting && (
+        <div
+          style={{
+            position: 'absolute',
+            border: '1px solid yellowgreen',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            left: Math.min(selectionArea.startX, selectionArea.endX),
+            top: Math.min(selectionArea.startY, selectionArea.endY),
+            width: Math.abs(selectionArea.endX - selectionArea.startX),
+            height: Math.abs(selectionArea.endY - selectionArea.startY),
+          }}
+        />
+      )}
+
+      {/* Initial "deck" of cards */}
+      {cards.length > 0 &&
+        cards.slice(0, 23).map((card, index) => {
+          return (
+            <Card
+              key={card.id + index}
+              card={card}
+              initialCoordinates={{ x: 5, y: 470 }}
+              index={index}
+              isSelected={selectedCards.includes(card.id)}
+              dragOffset={dragOffset}
+              dragStartPositions={dragStartPositions}
+            />
+          );
+        })}
+    </div>
+  );
 }
 
 export default Board;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,200 +3,343 @@ import React from 'react';
 import type { MagicApiCardResponse } from 'src/types';
 
 const Card = (props: {
-	card: MagicApiCardResponse;
-	initialCoordinates: { x: number; y: number };
-	index: number;
-	isSelected: boolean;
-	dragOffset: { x: number; y: number };
-	dragStartPositions: { [key: string]: { x: number; y: number } } | undefined;
+  card: MagicApiCardResponse;
+  initialCoordinates: { x: number; y: number };
+  index: number;
+  isSelected: boolean;
+  dragOffset: { x: number; y: number };
+  dragStartPositions: { [key: string]: { x: number; y: number } } | undefined;
 }) => {
-	const cardRef = React.useRef<HTMLDivElement>(null);
-	const [position, setPosition] = React.useState(props.initialCoordinates);
-	const [isTapped, setIsTapped] = React.useState(false);
-	const [isFaceDown, setIsFaceDown] = React.useState(true);
+  const cardRef = React.useRef<HTMLDivElement>(null);
+  const [position, setPosition] = React.useState(props.initialCoordinates);
+  const [isTapped, setIsTapped] = React.useState(false);
+  const [isFaceDown, setIsFaceDown] = React.useState(true);
 
-	const [isMouseOver, setIsMouseOver] = React.useState(false);
-	const [isMouseDown, setIsMouseDown] = React.useState(false);
-	const [isBeingZoomed, setIsBeingZoomed] = React.useState(false);
-	const [dragStart, setDragStart] = React.useState(props.initialCoordinates);
-	const [zIndex, setZIndex] = React.useState(props.index);
+  const [isMouseOver, setIsMouseOver] = React.useState(false);
+  const [isMouseDown, setIsMouseDown] = React.useState(false);
+  const [isBeingZoomed, setIsBeingZoomed] = React.useState(false);
+  const [dragStart, setDragStart] = React.useState(props.initialCoordinates);
+  const [zIndex, setZIndex] = React.useState(props.index);
 
-	function handleMouseEnter() {
-		setIsMouseOver(true);
-		cardRef.current?.focus();
-	}
+  function handleMouseEnter() {
+    setIsMouseOver(true);
+    cardRef.current?.focus();
+  }
 
-	function handleMouseLeave() {
-		setIsMouseOver(false);
-		setIsMouseDown(false);
-		cardRef.current?.blur();
-	}
+  function handleMouseLeave() {
+    setIsMouseOver(false);
+    setIsMouseDown(false);
+    cardRef.current?.blur();
+  }
 
-	const handleMouseDown = (e: React.MouseEvent) => {
-		if (e.button == 2) {
-			e.preventDefault();
-			setIsBeingZoomed(!isBeingZoomed);
-			return;
-		}
-		setIsMouseDown(true);
-		setDragStart({ x: e.clientX - position.x, y: e.clientY - position.y });
-	};
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (e.button == 2) {
+      e.preventDefault();
+      setIsBeingZoomed(!isBeingZoomed);
+      return;
+    }
+    setIsMouseDown(true);
+    setIsMouseOver(false);
+    setDragStart({ x: e.clientX - position.x, y: e.clientY - position.y });
+  };
 
-	const handleMouseMove = (e: React.MouseEvent) => {
-		if (isMouseDown) {
-			e.preventDefault();
-			requestAnimationFrame(() => {
-				setPosition({
-					x: e.clientX - dragStart.x,
-					y: e.clientY - dragStart.y,
-				});
-			});
-		}
-	};
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (isMouseDown) {
+      e.preventDefault();
+      setZIndex(101);
+      requestAnimationFrame(() => {
+        setPosition({
+          x: e.clientX - dragStart.x,
+          y: e.clientY - dragStart.y,
+        });
+      });
+    }
+  };
 
-	const handleMouseUp = () => {
-		setIsMouseDown(false);
-	};
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+    setZIndex(1);
+  };
 
-	function handleDoubleClick() {
-		setIsTapped(!isTapped);
-	}
+  function handleDoubleClick() {
+    setIsTapped(!isTapped);
+  }
 
-	function handleKeyDown(e: React.KeyboardEvent) {
-		switch (e.key) {
-			case 't':
-				setIsTapped(!isTapped);
-				break;
-			case 'f':
-				setIsFaceDown(!isFaceDown);
-				break;
-			case 'ArrowUp':
-				zIndex < 100 && setZIndex(zIndex + 1);
-				break;
-			case 'ArrowDown':
-				zIndex > 1 && setZIndex(zIndex - 1);
-				break;
-			default:
-				break;
-		}
-	}
+  function handleKeyDown(e: React.KeyboardEvent) {
+    switch (e.key) {
+      case 't':
+        setIsTapped(!isTapped);
+        break;
+      case 'f':
+        setIsFaceDown(!isFaceDown);
+        break;
+      // case 'ArrowUp':
+      // 	e.preventDefault();
+      // 	zIndex < 100 && setZIndex(zIndex + 1);
+      // 	break;
+      // case 'ArrowDown':
+      // 	e.preventDefault();
+      // 	zIndex > 1 && setZIndex(zIndex - 1);
+      // 	break;
+      default:
+        break;
+    }
+  }
 
-	React.useEffect(() => {
-		if (!props.dragStartPositions) return;
-		if (props.isSelected) {
-			setZIndex(100);
-			// Calculate the drag offset for this card based on its initial position
-			const dragStartPos = props.dragStartPositions[props.card.id];
-			if (dragStartPos) {
-				setPosition({
-					x: dragStartPos.x + props.dragOffset.x,
-					y: dragStartPos.y + props.dragOffset.y,
-				});
-			}
-		}
-	}, [props.dragOffset]);
+  React.useEffect(() => {
+    if (!props.dragStartPositions) return;
+    if (props.isSelected) {
+      setZIndex(100);
+      // Calculate the drag offset for this card based on its initial position
+      const dragStartPos = props.dragStartPositions[props.card.id];
+      if (dragStartPos) {
+        setPosition({
+          x: dragStartPos.x + props.dragOffset.x,
+          y: dragStartPos.y + props.dragOffset.y,
+        });
+      }
+    }
+  }, [
+    props.card.id,
+    props.dragOffset,
+    props.dragStartPositions,
+    props.isSelected,
+  ]);
 
-	return (
-		<>
-			<div
-				data-element="CARD"
-				id={props.card.id}
-				tabIndex={0}
-				ref={cardRef}
-				onMouseEnter={handleMouseEnter}
-				onMouseDown={handleMouseDown}
-				onMouseMove={handleMouseMove}
-				onMouseUp={handleMouseUp}
-				onMouseLeave={handleMouseLeave}
-				onKeyDown={handleKeyDown}
-				onDoubleClick={handleDoubleClick}
-				style={{
-					position: 'absolute',
-					cursor: 'grab',
-					userSelect: 'none',
+  return (
+    <>
+      <div
+        data-element="CARD"
+        id={props.card.id}
+        tabIndex={0}
+        ref={cardRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+        onKeyDown={handleKeyDown}
+        onDoubleClick={handleDoubleClick}
+        style={{
+          position: 'absolute',
+          userSelect: 'none',
 
-					// card aspect
-					width: '150px',
-					aspectRatio: 1 / 1.4,
-					backgroundImage: `url(${props.card.imageUrl})`,
-					backgroundSize: 'contain',
-					backgroundRepeat: 'no-repeat',
-					backgroundPosition: 'center',
-					borderRadius: '10px',
+          // card aspect
+          fontFamily: 'garamond',
+          width: '150px',
+          aspectRatio: 1 / 1.4,
+          // Original card image is set as the backgroundImage of the main container
+          backgroundImage: `url(${props.card.imageUrl})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          borderRadius: '10px',
 
-					// card 3d position
-					top: position.y,
-					left: position.x,
-					zIndex: zIndex,
+          // card 3d position
+          top: position.y,
+          left: position.x,
+          zIndex: zIndex,
 
-					// card style states
-					...(isTapped && { transform: 'rotate(-30deg)' }),
-					...(isFaceDown && { backgroundImage: 'url(public/card_back.jpg)' }),
-					...(isMouseOver && { outline: '2px solid cyan' }),
-					...(isMouseDown && { cursor: 'grabbing', zIndex: 100 }),
-					...(props.isSelected && { outline: '2px solid yellowgreen' }),
-				}}
-			>
-				{!props.card.imageUrl && !isFaceDown && (
-					<div
-						style={{
-							backgroundColor: 'white',
-							borderRadius: '10px',
-							fontSize: '10px',
-							height: '100%',
-							padding: '5px',
-							display: 'flex',
-							flexDirection: 'column',
-						}}
-					>
-						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-							<p style={{ fontWeight: '800' }}>{props.card.name}</p>
-							<p>{props.card.manaCost}</p>
-						</div>
-						<p>{props.card.type}</p>
-						<p
-							style={{
-								border: '1px solid gray',
-								padding: '5px',
-								height: '100%',
-							}}
-						>
-							{props.card.text}
-						</p>
-						<p
-							style={{
-								fontSize: '16px',
-								fontWeight: '800',
-								textAlign: 'right',
-							}}
-						>
-							{props.card.power}/{props.card.toughness}
-						</p>
-					</div>
-				)}
-			</div>
-			{isBeingZoomed && (
-				<div
-					onMouseDown={() => setIsBeingZoomed(false)}
-					style={{
-						width: '400px',
-						aspectRatio: 1 / 1.4,
-						backgroundImage: `url(${props.card.imageUrl})`,
-						backgroundSize: 'contain',
-						backgroundRepeat: 'no-repeat',
-						backgroundPosition: 'center',
-						borderRadius: '20px',
-						position: 'absolute',
-						top: '20vh',
-						left: '40vw',
-						cursor: 'pointer',
-						transition: 'all 0.1s ease-in-out',
-						zIndex: 1000,
-					}}
-				></div>
-			)}
-		</>
-	);
+          // card style states
+          ...(isTapped && { transform: 'rotate(-30deg)' }),
+          ...(isFaceDown && { backgroundImage: 'url(public/card_back.jpg)' }),
+          ...(isMouseOver && { outline: '4px solid cyan' }),
+          ...(isMouseDown && { cursor: 'grabbing', zIndex: 100 }),
+          // ...(isMouseOver && !isMouseDown && { transform: 'scale(2)' }),
+          ...(props.isSelected && { outline: '4px solid yellowgreen' }),
+        }}
+      >
+        {/* If no image provided display raw data */}
+
+        {/* Maint raw data container */}
+        {!props.card.imageUrl && !isFaceDown && (
+          <div
+            style={{
+              backgroundColor: 'white',
+              borderRadius: '10px',
+              height: '100%',
+              padding: '10px',
+              display: 'flex',
+              flexDirection: 'column',
+              fontFamily: 'arial',
+              fontSize: '12px',
+            }}
+          >
+            {/* Card header container */}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height: '20px',
+              }}
+            >
+              {/* Card Name */}
+              <p style={{ fontWeight: '800' }}>{props.card.name}</p>
+
+              {/* Mana Cost */}
+              <div style={{ marginLeft: 'auto' }}>
+                {/* Generic */}
+                {props.card.manaCost.replace(/\D+/g, '')}
+                {/* Colored */}
+                {props.card.manaCost
+                  .replace(/[{}\d]/g, '')
+                  .replace(/W/g, '🌞')
+                  .replace(/U/g, '💧')
+                  .replace(/B/g, '💀')
+                  .replace(/G/g, '🌳')
+                  .replace(/R/g, '🌋')}
+              </div>
+            </div>
+
+            {/* Card Type */}
+            <p
+              style={{
+                marginTop: 'auto',
+              }}
+            >
+              {props.card.type}
+            </p>
+
+            {/* Card Text */}
+            <p
+              style={{
+                border: '1px solid gray',
+                padding: '5px',
+                height: 'fit-content',
+                lineHeight: '1.1',
+              }}
+            >
+              {props.card.text}
+            </p>
+
+            {/* Creature Stats */}
+            {props.card.power && (
+              <p
+                style={{
+                  fontSize: '16px',
+                  fontWeight: '800',
+                  textAlign: 'right',
+                }}
+              >
+                {props.card.power}/{props.card.toughness}
+              </p>
+            )}
+          </div>
+        )}
+      </div>
+
+      {/* Big preview on hover */}
+      {isMouseOver && !isFaceDown && !isMouseDown && (
+        <div
+          style={{
+            width: '20vw',
+            aspectRatio: '1 / 1.4',
+            ...(props.card.imageUrl
+              ? {
+                  backgroundImage: `url(${props.card.imageUrl})`,
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                }
+              : {
+                  backgroundColor: 'white',
+                  fontSize: '20px',
+                  padding: '15px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  outline: '10px solid black',
+                  outlineOffset: '-10px',
+                }),
+            lineHeight: '1.1',
+            borderRadius: '16px',
+            position: 'fixed',
+            cursor: 'pointer',
+            transition: 'all 0.1s ease-in-out',
+            zIndex: 1000,
+            pointerEvents: 'none',
+          }}
+        >
+          {/* If no image available display raw data */}
+
+          {/* Main raw data container */}
+          {!props.card.imageUrl && (
+            <div
+              style={{
+                backgroundColor: 'white',
+                borderRadius: '6px',
+                fontSize: '15px',
+                height: '100%',
+                padding: '5px',
+                display: 'flex',
+                flexDirection: 'column',
+                fontFamily: 'arial',
+              }}
+            >
+              {/* Card Header */}
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <p style={{ fontWeight: '800' }}>{props.card.name}</p>
+
+                {/* Mana Cost */}
+                <div style={{ marginLeft: 'auto' }}>
+                  {/* Generic */}
+                  {props.card.manaCost.replace(/\D+/g, '')}
+                  {/* Colored */}
+                  {props.card.manaCost
+                    .replace(/[{}\d]/g, '')
+                    .replace(/W/g, '🌞')
+                    .replace(/U/g, '💧')
+                    .replace(/B/g, '💀')
+                    .replace(/G/g, '🌳')
+                    .replace(/R/g, '🌋')}
+                </div>
+              </div>
+
+              {/* Card Type */}
+              <p
+                style={{
+                  marginTop: 'auto',
+                }}
+              >
+                {props.card.type}
+              </p>
+
+              {/* Card Text */}
+              <p
+                style={{
+                  border: '1px solid gray',
+                  padding: '5px',
+                  height: 'fit-content',
+                  lineHeight: '1.1',
+                }}
+              >
+                {props.card.text}
+              </p>
+
+              {/* Creature Stats */}
+              {props.card.power && (
+                <p
+                  style={{
+                    fontSize: '16px',
+                    fontWeight: '800',
+                    textAlign: 'right',
+                  }}
+                >
+                  {props.card.power}/{props.card.toughness}
+                </p>
+              )}
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Card;
